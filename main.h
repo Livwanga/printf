de1a6b3be68cefc3e5ffe85655b5e5f074ae83ba
livwanga <owangala@gmail.com>
2023-04-25 02:06:14 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- main.h
+++ main.h
@@ -39,7 +39,6 @@ struct fmt
  */
 typedef struct fmt fmt_t;
 
-int putchar(int c);
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
@@ -47,6 +46,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
+int _putchar(char c);
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
