afdb43189e9934179e6c0a8a0c9442f26332d53f
livwanga <owangala@gmail.com>
2023-04-25 02:33:33 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- main.h
+++ main.h
@@ -46,7 +46,6 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
-int _putchar(char c);
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
